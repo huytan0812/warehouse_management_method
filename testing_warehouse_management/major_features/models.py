e18cc81aff584f50779ce8dbc42921ee9e2f86f5
Huy Tan <huytan12c6@gmail.com>
2023-04-14 13:47:51 +0700

    New updates

--- testing_warehouse_management/major_features/models.py
+++ testing_warehouse_management/major_features/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class WarehouseManagementMethod(models.Model):
+    name = models.TextField(null=False, blank=False, default="", max_length=1000)
+
+    def __str__(self):
+        return f"{self.name}"
--- testing_warehouse_management/major_features/views.py
+++ testing_warehouse_management/major_features/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from . models import *
 
 # Create your views here.
 def index(request):
-    pass
+    warehouse_management_methods = WarehouseManagementMethod.objects.all()
+    context = {
+        'warehouse_management_methods': warehouse_management_methods
+    }
+
+    return render(request, "major_features/index.html", context)
