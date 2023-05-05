7cca57f74bacc020233d9b1dbc13367dfa8ef9e6
Huy Tan <huytan12c6@gmail.com>
2023-05-05 13:19:02 +0700

    Configuring for security testing by OWASP ZAP

--- testing_warehouse_management/testing_warehouse_management/settings.py
+++ testing_warehouse_management/testing_warehouse_management/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# For security testing using OWASP ZAP
+os.environ['http_proxy'] = 'http://127.0.0.1:8000'
+os.environ['https_proxy'] = 'http://127.0.0.1:8000'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
