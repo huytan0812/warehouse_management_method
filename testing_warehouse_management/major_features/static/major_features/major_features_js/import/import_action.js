79bce718fc4ffd90003e41e6d76e1016b2e6f259
Huy Tan <huytan12c6@gmail.com>
2023-05-09 21:41:50 +0700

    Adding JavaScript

--- testing_warehouse_management/major_features/static/major_features/major_features_js/import/import_action.js
+++ testing_warehouse_management/major_features/static/major_features/major_features_js/import/import_action.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Hello World");
-    let newImportPurchaseForm = document.getElementById("new-import-purchase-row");
-    console.log(newImportPurchaseForm);
+    let importPurchaseFormsContainer = document.getElementById("import-purchase-forms-container");
+    let lastImportPurchaseForm = document.getElementById("new-import-purchase-row");
+    const addPurchaseForm = document.getElementById("add-purchase-form");
+
+    addPurchaseForm.addEventListener("click", () => {
+        const newImportPurchaseForm = document.createElement("tr");
+        newImportPurchaseForm.innerHTML = lastImportPurchaseForm.innerHTML;
+        lastImportPurchaseForm.removeAttribute("id");
+        newImportPurchaseForm.setAttribute("class", "import-purchase-form");
+        newImportPurchaseForm.setAttribute("id", "new-import-purchase-row");
+
+        importPurchaseFormsContainer.append(newImportPurchaseForm);
+    })
 })
