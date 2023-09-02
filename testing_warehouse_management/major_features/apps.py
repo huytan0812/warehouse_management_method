79d807bb3b32da48deaccaae5668e2ad0e4c50c0
Huy Tan <huytan12c6@gmail.com>
2023-09-03 00:00:54 +0700

    Fix bug for signal not running

--- testing_warehouse_management/major_features/apps.py
+++ testing_warehouse_management/major_features/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_delete
 
 class MajorFeaturesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'major_features'
 
     def ready(self):
-        import testing_warehouse_management.major_features.signals
+        from . import signals
--- testing_warehouse_management/major_features/signals.py
+++ testing_warehouse_management/major_features/signals.py
@@ -2,7 +2,7 @@ from . models import *
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
-@receiver(post_delete, model=ExportOrderDetail)
+@receiver(post_delete, sender=ExportOrderDetail)
 def return_quantity_for_import_purchase(sender, instance, **kwargs):
     involving_import_purchase = instance.import_purchase_id
     involving_import_purchase_quantity_remain = involving_import_purchase.quantity_remain + instance.quantity_take
