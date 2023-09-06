028eaa99520beda89ac17e2d524772ce978f23b8
Huy Tan <huytan12c6@gmail.com>
2023-09-06 21:19:12 +0700

    Set up logic for FIFO & LIFO'

--- testing_warehouse_management/major_features/warehouse_management_methods.py
+++ testing_warehouse_management/major_features/warehouse_management_methods.py
@@ -23,12 +23,48 @@ def FIFO(export_order_obj):
         quantity_remain__gt=0
         ).order_by('import_shipment_id__date', 'id')
 
+    quantity_export_remain = export_order_obj.quantity_export
+
+    for purchase in import_purchases:
+        if purchase.quantity_remain >= quantity_export_remain:
+            export_order_detail_obj = ExportOrderDetail.objects.create(
+                export_order_id=export_order_obj,
+                import_purchase_id=purchase,
+                quantity_take=quantity_export_remain
+            )
+            return
+        
+        quantity_export_remain -= purchase.quantity_remain
+        export_order_detail_obj = ExportOrderDetail.objects.create(
+            export_order_id=export_order_obj,
+            import_purchase_id=purchase,
+            quantity_take=purchase.quantity_remain
+        )
+
 def LIFO(export_order_obj):
     import_purchases = ImportPurchase.objects.select_related('import_shipment_id', 'product_id').filter(
         import_shipment_id__total_shipment_value__gt=0,
         product_id__name=export_order_obj.product_id.name,
         quantity_remain__gt=0
         ).order_by('-import_shipment_id__date', '-id')
+
+    quantity_export_remain = export_order_obj.quantity_export
+
+    for purchase in import_purchases:
+        if purchase.quantity_remain >= quantity_export_remain:
+            export_order_detail_obj = ExportOrderDetail.objects.create(
+                export_order_id=export_order_obj,
+                import_purchase_id=purchase,
+                quantity_take=quantity_export_remain
+            )
+            return
+        
+        quantity_export_remain -= purchase.quantity_remain
+        export_order_detail_obj = ExportOrderDetail.objects.create(
+            export_order_id=export_order_obj,
+            import_purchase_id=purchase,
+            quantity_take=purchase.quantity_remain
+        )
 
 def average_method_constantly(export_order_obj):
     pass
